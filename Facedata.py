08e86d4c3372cb0a8328f3f52d799c7cd40f5c11
kingsaint <rajarshi.kingsaint.bhowmik@gmail.com>
2015-11-16 00:49:25 -0500

    Data Loading into memory

---
+++ Facedata.py
@@ -0,0 +1,20 @@
+#!/usr/bin/py
+
+class Facedata:
+
+	def __init__(self,obj_id,image,label):
+		self.obj_id = obj_id
+		self.image = image
+		self.label = label
+
+class Digitdata:
+
+	def __init__(self,obj_id,image,label):
+		self.obj_id = obj_id
+		self.image = image
+		self.label = label
+
+
+
+
+
